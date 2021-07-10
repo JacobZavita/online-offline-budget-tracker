816a7ef475af96f12690de761c0c8219ef01e7aa
Jacob Zavita <jacobzavita@gmail.com>
2021-07-09 17:24:29 -0700

    add service-worker

---
+++ public/service-worker.js
@@ -0,0 +1,29 @@
+const urls = [
+  '/',
+  '/index.html',
+  '/manifest.json',
+  'icons/icon-192x192.png',
+  'icons/icon-512x512.png'
+]
+
+self.addEventListener('install', event => {
+  event.waitUntil(
+    caches.open('menu-cache-v1')
+      .then(cache => cache.addAll(urls)))
+})
+
+self.addEventListener('fetch', event => {
+  event.respondWith(
+    fetch(event.request)
+      .catch(err => {
+        console.log(err)
+        return caches.match(event.request).then(match => {
+          if (match) {
+            return match
+          } else if (event.request.headers.get('accept').includes('text/html')) {
+            return caches.match('/')
+          }
+        })
+      })
+  )
+})
